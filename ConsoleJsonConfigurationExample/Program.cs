62a09b9bf22effc7d0a12c46d364b5331a94592c
StevenBuglione <steven.buglione1@marist.edu>
2021-07-12 17:53:21 -0400

    Added Json Configuration To Console Application

---
+++ ConsoleJsonConfigurationExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ConsoleJsonConfigurationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environment}.json", true, true)
+                .AddEnvironmentVariables();
+
+            var configurationRoot = builder.Build();
+            var appConfig = configurationRoot.GetSection(nameof(AppConfig)).Get<AppConfig>();
+
+            Console.WriteLine(appConfig.Environment);
+        }
+    }
+}
